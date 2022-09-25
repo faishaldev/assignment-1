4a6c98bccb8927ef0903d494bff82cd562f61e66
Musthafa Faishal <musthafafaishal@gmail.com>
2022-09-26 04:19:01 +0700

    update

--- biodata.go
+++ biodata.go
@@ -12,6 +12,11 @@ func main() {
 
 	if len(os.Args) != 1 {
 		for i := 1; i < len(os.Args); i++ {
+			if helpers.IsLetter(os.Args[i]) {
+				fmt.Printf("\nMasukan nomor absen %v tidak valid!\n", os.Args[i])
+				continue
+			}
+
 			absen, _ := strconv.Atoi(os.Args[i])
 
 			if absen <= 0 {
--- helpers/function.go
+++ helpers/function.go
@@ -2,7 +2,18 @@ package helpers
 
 import (
 	"fmt"
+	"unicode"
 )
+
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+
+	return true
+}
 
 func Print(biodata []Biodata, absen int) {
 	fmt.Println("\nNama\t\t\t\t: ", biodata[absen].Nama)
